1c0469fd9d06ad5ad2fb201d3dfdd8b31dd140c6
ArmoryCup <farsh03@gmail.com>
2015-09-06 19:51:04 -0600

    final changes

--- pa1.cpp
+++ pa1.cpp
@@ -1,45 +1,37 @@
-//
-// Created by Ivo Georgiev on 8/25/15.
-//
-
-#include "Point.h"
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include "Point.h"
 
 using namespace std;
 
 // This function calculate area of a triangle by using Heron's formula.
-double computeArea(Point &a, Point &b, Point &c){
-	
-	double sideA, sideB, sideC;					// sides of a trinagle
-	double semiperimeter;								// semiperimeter of a triangle
-	double area;												// to hold area of a triangle
+double computeArea(Point &a, Point &b, Point &c) {
 
-	// calculate the lengths of the triangle
-	sideA = a.distanceTo(b);
-	sideB = b.distanceTo(c);
-	sideC= c.distanceTo(a);
+    double sideA, sideB, sideC;                    // sides of a trinagle
+    double semiperimeter;                          // semiperimeter of a triangle
+    double area;                                   // to hold area of a triangle
 
-	cout << "SideA: " << sideA << endl;
-	cout << "SideB: " << sideB << endl;
-	cout << "SideC: " << sideC << endl;
+    // calculate the lengths of the triangle
+    sideA = a.distanceTo(b);
+    sideB = b.distanceTo(c);
+    sideC = c.distanceTo(a);
 
-	// calculate semiperimeter and area of the triangle by using
-	// Heron's formula
-	semiperimeter = (sideA + sideB + sideC)/2;
-	area = sqrt(semiperimeter*(semiperimeter - sideA)*(semiperimeter - sideB)*(semiperimeter - sideC));
-	return area;
+    // calculate semiperimeter and area of the triangle by using
+    // Heron's formula
+    semiperimeter = (sideA + sideB + sideC) / 2;
+    area = sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
+    return area;
 
 }
 
 
 int main(void) {
-    Point p1(3, 2), p2(4, 2), p3(-8,2);
-		
-		// Display the area of a triangle in double-precision floating point number
-		cout << "Area = ";
-		cout << fixed << setprecision(2) << computeArea(p1, p2, p3) << endl;
+    Point p1(3, -5), p2(-7, 2), p3(-8, 2);
+
+    // Display the area of a triangle in double-precision floating point number
+    cout << "Area of the triangle = ";
+    cout << fixed << setprecision(2) << computeArea(p1, p2, p3) << endl;
 
     return 0;
 }
